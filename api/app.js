d498c3a9cf3557777d51d964156c2fa7dcf4310d
laurabdier <laura.boudier@coding-academy.fr>
2020-10-01 23:52:48 +0200

    connected to local mongo db for first tests

--- api/app.js
+++ api/app.js
@@ -1,5 +1,37 @@
 const express = require('express');
+var mongoose = require("mongoose");
+const morgan = require("morgan");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+const uri = 'mongodb+srv://lauraNovoResume:Novoresume2020@challengenovo.echg3.mongodb.net/challengeNovoresume?retryWrites=true&w=majority';
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.log("err connection db:", err);
+})
+
+db.once('open', () => {
+  console.log("db connection succeeded!")
+})
+
 const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 9007
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port : ${PORT}`)
+})
+
+/*
+
+const AuthRoute = require("./src/routes/auth")
+
 const connectDb = require("./src/connection");
 
 const PORT = 9007;
@@ -18,3 +50,5 @@ app.listen(PORT, () => {
     console.log("MongoDb connected");
   });
 });
+
+app.use('/auth', AuthRoute) */
