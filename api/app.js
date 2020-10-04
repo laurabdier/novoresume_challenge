8493979fb0dfca6db4973cf1440ccca10eb92d33
laurabdier <laura.boudier@coding-academy.fr>
2020-10-04 21:06:47 +0200

    installed router, controllers, CORS

--- api/app.js
+++ api/app.js
@@ -4,7 +4,16 @@ const morgan = require("morgan");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 
-const uri = 'mongodb+srv://lauraNovoResume:Novoresume2020@challengenovo.echg3.mongodb.net/challengeNovoresume?retryWrites=true&w=majority';
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const authRoute = require("./src/routes/usersRoute");
+const productRoute = require("./src/routes/productsRoute");
+
+const uri = 'mongodb+srv://lauraNovoResume:Novoresume2020@challengenovo.echg3.mongodb.net/challengeNovoresume?retryWrites=true&w=majority'
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 
@@ -16,39 +25,12 @@ db.once('open', () => {
   console.log("db connection succeeded!")
 })
 
-const app = express();
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 const PORT = process.env.PORT || 9007
 
 app.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`)
 })
 
-/*
-
-const AuthRoute = require("./src/routes/auth")
-
-const connectDb = require("./src/connection");
-
-const PORT = 9007;
-
-app.get('/', (req, res) => {
-    res.send('Hello from Novoresume !');
-});
-
-app.get("/users", (req, res) => {
-  res.send("Get users \n");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-  connectDb().then(() => {
-    console.log("MongoDb connected");
-  });
-});
-
-app.use('/auth', AuthRoute) */
+app.use(cors())
+app.use('/auth', authRoute);
+app.use('/product', productRoute)
